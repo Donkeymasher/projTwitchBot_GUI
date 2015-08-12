58d79fac5347be705f2ddc9fdcd16070eaec5a81
Donkeymasher <david.poots1@btinternet.com>
2015-08-12 22:51:14 +0100

    database implemintatiob

--- projTwitchBot_GUI/FileReaderWriter.cs
+++ projTwitchBot_GUI/FileReaderWriter.cs
@@ -120,9 +120,29 @@ namespace ProjTwitchBotVisual
             SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
             m_dbConnection.Open();
 
-            string sql = "create table highscores (name varchar(20), score int)";
+            string sql = "create table highscores (name Varchar(20), score int)";
+            
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             command.ExecuteNonQuery();
+
+            sql = "create table commands (name Varchar(45), score int)";
+
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 3000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            m_dbConnection.Close();
         }
     }
 }
